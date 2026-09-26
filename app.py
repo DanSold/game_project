6aecc58c2a99f64b805732b44cbd713351a1ae78
danKo <dansoldat9@gmail.com>
2026-09-26 18:01:35 +0200

    units_lib.py: class Player -> new self param equipped_weapon, equipped_armor and quests_items. new class Item -> with inheritance classes Potion, Weapon and Armor. A pair of instances has been added for each class. The `shop_inventory` variable was created for the shop usage mechanics.
    locations.py:
    Two new locations have been added (the Old Ruins and the Bandit Camp). The Old Ruins location is not currently being used.
    
    The village location has been improved and
    now functions as the main location, with a
    shop and a blacksmith.
    
    The location dialogue windows are not complete yet and still need further development.
    
    func.py:
    Basic functions have been added, such as:
    pause, battle, healing (is used in battle and chek_inventory function), blacksmith (interaction with equipments), open_shop (sell or buy items and equipments), check_level_up (is used in battle func), chek_param, chek_inventory, chek_quests, (unequip_weapon, unequip_armor, equip_item,
    upgrade_gear are used in blacksmith function)

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
+from func import pause, chek_inventory, chek_quests, chek_param
 from units_lib import Player
-from locations import village
+from locations import village, bandit_camp, old_ruins
+
 
 def main():
     print("=" * 55)
     print(" THE LAST FLAME")
     print("=" * 55)
     print("Now the dragon is awakening.")
-    print("Five ancient artifacts are the only things " "that can stop what is coming.")
+    print("Five ancient artifacts are the only things that can stop what is coming.")
 
     name = input("Enter your name: ")
     player = Player(name)
@@ -14,21 +16,55 @@ def main():
 
     while player.health > 0:
 
-        print("\n" + "=" * 45)
+        print("=" * 45)
         print("MAIN MENU")
         print("=" * 45)
 
         print("1. Visit Eldoria Village")
+        print("2. Visit Bandit Camp")
+        print("3. Old Ruins")
+        print("4. Check your parameters and stats")
+        print("5. Check your inventory")
+        print("6. Chek quests")
         print("0. Quit game")
 
         choice = input("Choose destination: ")
+
         if choice == "1":
-            village()
+            village(player)
+
+        elif choice == "2":
+            bandit_camp(player)
+
+        elif choice == "3":
+            old_ruins(player)
+
+        elif choice == "4":
+            chek_param(player)
+
+        elif choice == "5":
+            chek_inventory(player)
+
+        elif choice == "6":
+            chek_quests(player)
+
         elif choice == "0":
             print("Thank you for playing The Last Flame!")
             break
+
         else:
             print("Invalid choice.")
+            pause()
+
+    if player.health <= 0:
+        print("=" * 45)
+        print("GAME OVER")
+        print("=" * 45)
+
+        print(
+            "Your journey has come to an end."
+        )
+
 
 if __name__ == "__main__":
     main()
